0b3082265a20f9684a6e0b4173c903c3be5e9d05
Ángel Lavandeira <angellavandeira@pc.home>
2014-12-22 08:52:40 +0100

    Cambios

---
+++ js/contacto.js
@@ -0,0 +1,26 @@
+$('#contacto').on('pageshow', function(event, data) {
+	$('#form_contact').show();
+});
+
+
+   //*****----- CONTACTO -----*****
+   function contactar () {
+	ur = 'reservas/contacto.php?callback=?';
+	var user_activo = localStorage.getItem("id_user_app_movil");
+	var id_tienda = localStorage.getItem("id_centro");
+	
+	/*--RECOGIDA DE DATOS--*/
+	var nombre = $('#form_contact #nombre').val();
+	var telefono = $('#form_contact #telefono').val();
+	var email = $('#form_contact #email').val();
+	var mensaje = $('#form_contact #mensaje').val();
+	
+		$.getJSON(serviceURL + ur, { id_tienda:id_tienda, user_activo:user_activo, nombre:nombre, telefono:telefono, email:email, mensaje:mensaje }, function(data){
+			if (data.resultado === true) {
+	$('#form_contact').hide();
+	$('#res_contacto').html(data.respuesta);
+			} else {
+				alert (data.respuesta);
+			}
+		});
+   }
